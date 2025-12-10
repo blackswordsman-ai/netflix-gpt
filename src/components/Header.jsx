b1e371e211ff9adfa6957610668d23466e9c0d50
blackswordsman-ai <lansonthandappilly20@gmail.com>
2025-12-10 19:45:27 +0530

    done multi language feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,7 @@ const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch=useSelector((store) =>store.gpt.showGptSearch)
 
   const handleSignOut = () => {
     signOut(auth)
@@ -67,19 +68,20 @@ const Header = () => {
       <img className="w-44 " src={LGO} />
       {user && (
         <div className="flex p-2">
-          <select className="p-2 bg-gray-300 text-white" onChange={handleLanguageChange}>
+    {  showGptSearch &&(    
+      <select className="p-2 bg-gray-300 text-white" onChange={handleLanguageChange}>
             {SUPPORTED_LANGUAGES.map((lang) => (
               <option key={lang.identifier} value={lang.identifier}>
                 {lang.name}
               </option>
             ))}
-            \
-          </select>
+            
+          </select>)}
           <button
             className="p-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
             onClick={handleGptSearchClick}
           >
-            GPT Search
+            {showGptSearch? "Home" :"GPT Search"}
           </button>
           <img src={user?.photoURL} alt="user-icon" className="w-12 h-12" />
           <button onClick={handleSignOut} className="font-bold text-white">
